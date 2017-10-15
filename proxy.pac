203d6b93e42a43c09775a5d2675c81021d984de6
Hiram Silvey <blujay270@gmail.com>
2017-10-15 01:22:16 -0400

    attempted message passing, broken

--- proxy.pac
+++ proxy.pac
@@ -1,9 +1,21 @@
+let type = ""
+let host = ""
+let port = ""
+proxyDNS = false
+
 function FindProxyForURL(url, host) {
-  const msg = [{
-        type: "socks",
-        host: "localhost",
-        port: 9050,
-        proxyDNS: true
-  }]
-  return msg
+    const msg = [{
+        type: type,
+        host: host,
+        port: port,
+        proxyDNS: proxyDNS
+    }]
+    return msg
 }
+
+browser.runtime.onMessage.addListener((message) => {
+    type = message.type
+    host = message.host
+    port = message.port
+    proxyDNS = message.proxyDNS
+})
--- tortab.js
+++ tortab.js
@@ -1,4 +1,6 @@
-async function createContainer(){
+browser.proxy.register("proxy.pac")
+
+    async function createContainer(){
     const checkContainer = await browser.contextualIdentities.query({
         name: 'Tor'
     })
@@ -17,13 +19,27 @@ async function createContainer(){
 createContainer()
 
 browser.webRequest.onBeforeRequest.addListener(async function (details) {
+    console.log(JSON.stringify(details))
     if (details.tabId !== browser.tabs.TAB_ID_NONE) {
         const tab = await browser.tabs.get(details.tabId)
         const setting = await browser.storage.local.get([tab.cookieStoreId])
+        let messageToProxy = {}
         if (setting[tab.cookieStoreId]) {
-            browser.proxy.register("proxy.pac")
-            return {}
+            messageToProxy = {
+                type: "socks",
+                host: "localhost",
+                port: 9050,
+                proxyDNS: true
+            }
+        } else {
+            messageToProxy = {
+                type: details.proxyInfo.type,
+                host: details.proxyInfo.host,
+                port: details.proxyInfo.port,
+                proxyDNS: details.proxyInfo.proxyDNS
+            }
         }
+        await browser.runtime.sendMessage(messageToProxy, {toProxyScript: true})
     }
     return {}
 }, {urls: ["<all_urls>"]}, ["blocking"])
