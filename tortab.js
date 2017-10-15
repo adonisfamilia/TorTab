bdb4da7241150e73858129b76b642ad04ce2abbb
Hiram Silvey <blujay270@gmail.com>
2017-10-15 13:22:24 -0400

    each request is treated separately now depending if in tor container - works for keeping separate

--- tortab.js
+++ tortab.js
@@ -2,7 +2,7 @@ async function createContainer(){
     const checkContainer = await browser.contextualIdentities.query({
         name: 'Tor'
     })
-    if(checkContainer.length === 0){
+    if(checkContainer.length === 0) {
         const identity = await browser.contextualIdentities.create({
             name: 'Tor',
             icon: 'fingerprint',
@@ -17,13 +17,25 @@ async function createContainer(){
 createContainer()
 
 browser.webRequest.onBeforeRequest.addListener(async function (details) {
+    console.log('here')
     if (details.tabId !== browser.tabs.TAB_ID_NONE) {
         const tab = await browser.tabs.get(details.tabId)
         const setting = await browser.storage.local.get([tab.cookieStoreId])
         if (setting[tab.cookieStoreId]) {
-            browser.proxy.register("proxy.pac")
-            return {}
+            if (details.proxyInfo === null) {
+                await browser.proxy.register("proxy.pac")
+            } else {
+                return {}
+            }
+        } else {
+            if (details.proxyInfo !== null) {
+                await browser.proxy.unregister()
+            } else {
+                return {}
+            }
         }
+        const redirectUrl = details.url
+        return {redirectUrl}
     }
     return {}
 }, {urls: ["<all_urls>"]}, ["blocking"])
