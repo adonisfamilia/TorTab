9000b001e001704c758c732c61ba4dfc4f86479c
Adonis Familia <hello@adonisfamilia.com>
2017-10-15 00:16:30 -0400

    Added check for existing Tor container.

--- tortab.js
+++ tortab.js
@@ -1,1 +1,16 @@
 browser.proxy.register("proxy.pac")
+
+async function createContainer(){
+  const checkContainer = await browser.contextualIdentities.query({
+    name: 'Tor'
+  })
+  if(checkContainer.length === 0){
+    const identity = await browser.contextualIdentities.create({
+      name: 'Tor',
+      icon: 'fingerprint',
+      color: 'purple'
+    })
+  }
+}
+
+createContainer()
