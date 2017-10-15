026189c2060175f7dd3b6533e927e38a02e98a19
Brian Silvey <blujay270@gmail.com>
2017-10-14 20:10:43 -0400

    message passing added

--- TorTab.js
+++ TorTab.js
@@ -1,9 +1,15 @@
 browser.proxy.register("proxy.pac")
+
+browser.runtime.onMessage.addListener((message, sender) => {
+  if (sender.url === browser.extension.getURL("proxy.pac")) {
+    console.log(message);
+  }
+});
+
 let messageToProxy = {
-  enabled: true,
-  foo: "A string",
-  bar: 1234
-}
+    enabled: true,
+    foo: "A string",
+    bar: 1234
+};
 
 browser.runtime.sendMessage(messageToProxy, {toProxyScript: true});
-
