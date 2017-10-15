4fdc94234fe529a6018e7b17721da56cf25279a4
Hiram Silvey <blujay270@gmail.com>
2017-10-15 00:48:42 -0400

    containerized but then applies to everything

--- tortab.js
+++ tortab.js
@@ -1,16 +1,29 @@
-browser.proxy.register("proxy.pac")
-
 async function createContainer(){
-  const checkContainer = await browser.contextualIdentities.query({
-    name: 'Tor'
-  })
-  if(checkContainer.length === 0){
-    const identity = await browser.contextualIdentities.create({
-      name: 'Tor',
-      icon: 'fingerprint',
-      color: 'purple'
+    const checkContainer = await browser.contextualIdentities.query({
+        name: 'Tor'
     })
-  }
+    if(checkContainer.length === 0){
+        const identity = await browser.contextualIdentities.create({
+            name: 'Tor',
+            icon: 'fingerprint',
+            color: 'purple'
+        })
+        browser.storage.local.set({
+            [identity.cookieStoreId]: true
+        })
+    }
 }
 
 createContainer()
+
+browser.webRequest.onBeforeRequest.addListener(async function (details) {
+    if (details.tabId !== browser.tabs.TAB_ID_NONE) {
+        const tab = await browser.tabs.get(details.tabId)
+        const setting = await browser.storage.local.get([tab.cookieStoreId])
+        if (setting[tab.cookieStoreId]) {
+            browser.proxy.register("proxy.pac")
+            return {}
+        }
+    }
+    return {}
+}, {urls: ["<all_urls>"]}, ["blocking"])
